67dff3a476301ec8b3ab2d80e09f77f460cdf3fe
xnzar <mustafacetin2043@gmail.com>
2024-06-13 16:43:34 +0300

    lessonService-add

---
+++ src/main/java/com/mustafa/college_management/controller/LessonController.java
@@ -0,0 +1,38 @@
+package com.mustafa.college_management.controller;
+
+import com.mustafa.college_management.dto.LessonDto;
+import com.mustafa.college_management.service.impl.LessonServiceImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/lesson")
+public class LessonController {
+    private final LessonServiceImpl lessonService;
+    public LessonController(LessonServiceImpl lessonService) {
+        this.lessonService = lessonService;
+    }
+    @GetMapping
+    public ResponseEntity<List<LessonDto>> getAll(){
+        return new ResponseEntity<>(lessonService.getAll(), HttpStatus.OK);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<LessonDto> getLessonById(@PathVariable Long id){
+        return new ResponseEntity<>(lessonService.getById(id),HttpStatus.OK)
+    }
+    @PostMapping
+    public ResponseEntity<String> addLesson(@RequestBody LessonDto lessonDto){
+        return new ResponseEntity<>(lessonService.addLesson(lessonDto),HttpStatus.CREATED);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateLesson(@PathVariable Long id, @RequestBody LessonDto lessonDto){
+        return new ResponseEntity<>(lessonService.updateLesson(id,lessonDto),HttpStatus.OK);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteLesson(@PathVariable Long id){
+        return new ResponseEntity<>(lessonService.deleteLesson(id),HttpStatus.OK);
+    }
+}
